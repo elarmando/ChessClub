47e082c086363561ddc0cab121c7cd84bf331bb6
aserrato <aserrato@polyworksmexico.com>
2019-03-29 16:19:50 -0600

    tactics & rating

--- App.js
+++ App.js
@@ -22,6 +22,9 @@
         this.Name = "";
         this.UserName = "";
         this.DailyRating = "";
+        this.TacticsRating = null;
+        this.BlitzRating = null;
+        this.LastConnection = null;
         this.Games = [];
     }
 
@@ -68,6 +71,9 @@
                 member.Name = data.name;
                 member.UserName = data.username;
                 member.DailyRating = stats.chess_daily.last.rating;
+                member.TacticsRating = stats.tactics.highest == undefined ? 0 : stats.tactics.highest.rating ;
+                member.LastConnection = new Date(data.last_online * 1000);
+                member.BlitzRating = stats.chess_blitz == undefined || stats.chess_blitz.best == undefined? 0: stats.chess_blitz.best.rating;
 
                 GetGames(username, function(games){
                     member.Games = games.games;
@@ -119,13 +125,68 @@
         r.send();
     }
 
-    function CreateList()
+    function CreateTacticsRatingTable()
     {
         var unActive = [];
         var active = [];
+        var currentDateTime = new Date().getTime();
+        var dayTime = 1000*60*60*24;
+        var twoWeekTime = dayTime * 14;
+        var thresholdTime = twoWeekTime;
 
         memberObjs.forEach(function(e){
-            if(e.Games <= 0)
+            var timeElapsed = currentDateTime - e.LastConnection.getTime();
+
+            if(timeElapsed > thresholdTime)
+                unActive.push(e);
+            else
+                active.push(e);
+        });
+
+
+        active = active.sort(function(a,b){
+            return b.TacticsRating - a.TacticsRating;
+        });
+        
+        unActive = unActive.sort(function(a,b){
+            return b.TacticsRating - a.TacticsRating;
+        });
+
+       
+        var header = "<thead><tr><th>Position</th><th>User</th><th>Name</th><th>Highest Tactics Rating</th><th>Current Games</th><th>Last Connection</th></tr></thead>";
+       
+        var body = "";
+        var position = 0;
+
+        active.forEach(function(data, index){
+            position++;
+            body+= "<tr>"+"<td>"+(position )+"</td>" + "<td>"+data.UserName+"</td>"+ "<td>"+( (data.Name == undefined)? "" : data.Name) +"</td>"+ "<td>"+data.TacticsRating+"</td>"+ "<td>"+data.Games.length+"</td>"+"<td>"+data.LastConnection.toDateString()+"</td>"+"</tr>";          
+           
+        });
+
+        unActive.forEach(function(data, index){
+            position++;
+            body+= "<tr>"+"<td>"+(position )+"</td>" + "<td>"+data.UserName+"</td>"+ "<td>"+( (data.Name == undefined)? "" : data.Name) +"</td>"+ "<td>"+data.TacticsRating+"</td>"+ "<td>"+data.Games.length+"</td>"+"<td>"+data.LastConnection.toDateString()+ (" (Unactive)") +"</td>"+"</tr>";          
+        });
+        
+        body = "<tbody>"+ body +"</tbody>";
+
+        return "<table class='pure-table'>" + header + body + "</table>";
+    }
+
+    function CreateDailyRatingTable()
+    {
+        var unActive = [];
+        var active = [];
+        var currentDateTime = new Date().getTime();
+        var dayTime = 1000*60*60*24;
+        var twoWeekTime = dayTime * 14;
+        var thresholdTime = twoWeekTime;
+
+        memberObjs.forEach(function(e){
+            var timeElapsed = currentDateTime - e.LastConnection.getTime();
+
+            if(timeElapsed > thresholdTime)
                 unActive.push(e);
             else
                 active.push(e);
@@ -141,25 +202,86 @@
         });
 
        
-        var header = "<thead><tr><th>Position</th><th>User</th><th>Name</th><th>Daily Rating</th><th>Current Games</th></tr></thead>";
+        var header = "<thead><tr><th>Position</th><th>User</th><th>Name</th><th>Daily Rating</th><th>Current Games</th><th>Last Connection</th></tr></thead>";
        
         var body = "";
         var position = 0;
 
         active.forEach(function(data, index){
             position++;
-            body+= "<tr>"+"<td>"+(position )+"</td>" + "<td>"+data.UserName+"</td>"+ "<td>"+( (data.Name == undefined)? "" : data.Name) +"</td>"+ "<td>"+data.DailyRating+"</td>"+ "<td>"+data.Games.length+"</td>"+"</tr>";          
+            body+= "<tr>"+"<td>"+(position )+"</td>" + "<td>"+data.UserName+"</td>"+ "<td>"+( (data.Name == undefined)? "" : data.Name) +"</td>"+ "<td>"+data.DailyRating+"</td>"+ "<td>"+data.Games.length+"</td>"+"<td>"+data.LastConnection.toDateString()+"</td>"+"</tr>";          
            
         });
 
         unActive.forEach(function(data, index){
             position++;
-            body+= "<tr>"+"<td>"+(position )+"</td>" + "<td>"+data.UserName+"</td>"+ "<td>"+( (data.Name == undefined)? "" : data.Name) +"</td>"+ "<td>"+data.DailyRating+"</td>"+ "<td>"+data.Games.length+"</td>"+"</tr>";          
+            body+= "<tr>"+"<td>"+(position )+"</td>" + "<td>"+data.UserName+"</td>"+ "<td>"+( (data.Name == undefined)? "" : data.Name) +"</td>"+ "<td>"+data.DailyRating+"</td>"+ "<td>"+data.Games.length+"</td>"+"<td>"+data.LastConnection.toDateString()+ (" (Unactive)") +"</td>"+"</tr>";          
         });
         
-        body = "<tbody>"+ body +"</tbody>"
-          
-        window.document.body.innerHTML = "<table class='pure-table'>" + header + body + "</table>";
+        body = "<tbody>"+ body +"</tbody>";
+
+        return "<table class='pure-table'>" + header + body + "</table>";
+    }
+
+    function CreateBlitzRatingTable()
+    {
+        var unActive = [];
+        var active = [];
+        var currentDateTime = new Date().getTime();
+        var dayTime = 1000*60*60*24;
+        var twoWeekTime = dayTime * 14;
+        var thresholdTime = twoWeekTime;
+
+        memberObjs.forEach(function(e){
+            var timeElapsed = currentDateTime - e.LastConnection.getTime();
+
+            if(timeElapsed > thresholdTime)
+                unActive.push(e);
+            else
+                active.push(e);
+        });
+
+
+        active = active.sort(function(a,b){
+            return b.BlitzRating - a.BlitzRating;
+        });
+        
+        unActive = unActive.sort(function(a,b){
+            return b.BlitzRating - a.BlitzRating;
+        });
+
+       
+        var header = "<thead><tr><th>Position</th><th>User</th><th>Name</th><th>Blitz Rating</th><th>Current Games</th><th>Last Connection</th></tr></thead>";
+       
+        var body = "";
+        var position = 0;
+
+        active.forEach(function(data, index){
+            position++;
+            body+= "<tr>"+"<td>"+(position )+"</td>" + "<td>"+data.UserName+"</td>"+ "<td>"+( (data.Name == undefined)? "" : data.Name) +"</td>"+ "<td>"+data.BlitzRating+"</td>"+ "<td>"+data.Games.length+"</td>"+"<td>"+data.LastConnection.toDateString()+"</td>"+"</tr>";          
+           
+        });
+
+        unActive.forEach(function(data, index){
+            position++;
+            body+= "<tr>"+"<td>"+(position )+"</td>" + "<td>"+data.UserName+"</td>"+ "<td>"+( (data.Name == undefined)? "" : data.Name) +"</td>"+ "<td>"+data.BlitzRating+"</td>"+ "<td>"+data.Games.length+"</td>"+"<td>"+data.LastConnection.toDateString()+ (" (Unactive)") +"</td>"+"</tr>";          
+        });
+        
+        body = "<tbody>"+ body +"</tbody>";
+
+        return "<table class='pure-table'>" + header + body + "</table>";
+    }
+
+    function CreateList()
+    {
+       
+        var html1 = CreateDailyRatingTable();
+        var html2 = CreateTacticsRatingTable();
+        var html3 = CreateBlitzRatingTable();
+
+        var html = "<div>Daily Rating</div>" + html1 + "<div>Tactics Rating</div>" + html2 + "<div>Blitz Rating</div>" + html3;
+
+        window.document.body.innerHTML = html;
     }
 
     window.onload = Start;
